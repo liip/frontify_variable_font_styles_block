3fcd7cacf850f2b35d26fd774f1a1f6aa2a968d7
Marin Aeschbach <marin.aeschbach@gmail.com>
2022-11-25 11:22:02 +0100

    Remove unused components

--- src/library/printDimensionValue.ts
+++
@@ -1,7 +0,0 @@
-import { VariableFontDimension } from '../reducer';
-
-export const printDimensionValue = (dimension: VariableFontDimension) => {
-    return dimension.isValueRange
-        ? `${dimension.editorMinValue} - ${dimension.editorDefault} - ${dimension.editorMaxValue}`
-        : dimension.value;
-};
